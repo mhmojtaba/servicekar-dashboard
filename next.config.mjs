3cacd6e8007d9217f29c91a526a2d9eb72987cac
mhmojtaba <mhmojtaba@gmail.com>
2025-06-03 12:47:21 +0330

    [INIT]

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  trailingSlash: true,
+  output: "export",
+  basePath: "/",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
